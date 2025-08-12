e026b5a404719f7a33b16f4430a6b76a69fb45db
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-08-11 17:28:49 -0700

    Fast Push for Uploading Code to Remote

--- src/svd_slate.cpp
+++ src/svd_slate.cpp
@@ -40,27 +40,33 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int64_t m = 8192;
-    int64_t n = 4096;
-    std::vector<int64_t> nbs;
-    if (argc >= 3) {
-        m = std::stoll(argv[1]);
-        n = std::stoll(argv[2]);
-    }
-    if (argc >= 4) {
-        for (int i = 3; i < argc; ++i) nbs.push_back(std::stoll(argv[i]));
+    const int64_t nb = 192;
+
+    std::vector<std::pair<int64_t,int64_t>> cases;
+    if ((argc - 1) >= 2 && ((argc - 1) % 2 == 0)) {
+        for (int i = 1; i < argc; i += 2) {
+            int64_t m = std::stoll(argv[i]);
+            int64_t n = std::stoll(argv[i + 1]);
+            cases.emplace_back(m, n);
+        }
     } else {
-        nbs = {128, 192, 256, 320, 384, 512};
+        cases = {
+            {4096, 4096},
+            {8192, 4096},
+            {8192, 8192},
+            {12288, 6144},
+            {16384, 8192}
+        };
     }
 
     int p = 0, q = 0;
     best_pq(size, p, q);
 
     if (rank == 0) {
-        std::cout << "m n ranks p q nb seconds smax" << std::endl;
+        std::cout << "m n nb ranks p q seconds smax" << std::endl;
     }
 
-    for (int64_t nb : nbs) {
+    for (auto [m, n] : cases) {
         slate::Matrix<double> A(m, n, nb, p, q, MPI_COMM_WORLD);
         A.insertLocalTiles();
         fill_local_tiles(A);
@@ -74,14 +80,16 @@ int main(int argc, char** argv) {
         slate::svd(A, S, U, VT, { { slate::Option::Target, slate::Target::Devices } });
         MPI_Barrier(MPI_COMM_WORLD);
         double t1 = MPI_Wtime();
+
         double local = t1 - t0, global = 0.0;
         MPI_Reduce(&local, &global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
         if (rank == 0) {
             double smax = 0.0;
             for (double s : S) if (s > smax) smax = s;
-            std::cout << m << " " << n << " " << size << " " << p << " " << q
-                      << " " << nb << " " << global << " " << smax << std::endl;
+            std::cout << m << " " << n << " " << nb << " "
+                      << size << " " << p << " " << q << " "
+                      << global << " " << smax << std::endl;
         }
     }
 
