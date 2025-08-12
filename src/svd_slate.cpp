93aba7d44ec1ee9a68d405ff53b5ff459f360c2f
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-08-11 17:23:43 -0700

    Fast Push for Uploading Code to Remote

--- src/svd_slate.cpp
+++ src/svd_slate.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <algorithm>
 
 static void fill_local_tiles(slate::Matrix<double>& A) {
     for (int64_t j = 0; j < A.nt(); ++j) {
@@ -41,40 +42,50 @@ int main(int argc, char** argv) {
 
     int64_t m = 8192;
     int64_t n = 4096;
-    int64_t nb = 256;
+    std::vector<int64_t> nbs;
     if (argc >= 3) {
         m = std::stoll(argv[1]);
         n = std::stoll(argv[2]);
     }
     if (argc >= 4) {
-        nb = std::stoll(argv[3]);
+        for (int i = 3; i < argc; ++i) nbs.push_back(std::stoll(argv[i]));
+    } else {
+        nbs = {128, 192, 256, 320, 384, 512};
     }
 
     int p = 0, q = 0;
     best_pq(size, p, q);
 
-    slate::Matrix<double> A(m, n, nb, p, q, MPI_COMM_WORLD);
-    A.insertLocalTiles();
-    fill_local_tiles(A);
-
-    int64_t k = std::min<int64_t>(m, n);
-    std::vector<double> S(k);
-    slate::Matrix<double> U, VT;
-
-    slate::svd(A, S, U, VT, {
-        { slate::Option::Target, slate::Target::Devices }
-    });
-
     if (rank == 0) {
-        double smax = 0.0;
-        for (double s : S) if (s > smax) smax = s;
-        std::cout << "m=" << m << " n=" << n << " nb=" << nb
-                  << " ranks=" << size << " p=" << p << " q=" << q
-                  << " smax=" << smax << std::endl;
+        std::cout << "m n ranks p q nb seconds smax" << std::endl;
+    }
+
+    for (int64_t nb : nbs) {
+        slate::Matrix<double> A(m, n, nb, p, q, MPI_COMM_WORLD);
+        A.insertLocalTiles();
+        fill_local_tiles(A);
+
+        int64_t k = std::min<int64_t>(m, n);
+        std::vector<double> S(k);
+        slate::Matrix<double> U, VT;
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        double t0 = MPI_Wtime();
+        slate::svd(A, S, U, VT, { { slate::Option::Target, slate::Target::Devices } });
+        MPI_Barrier(MPI_COMM_WORLD);
+        double t1 = MPI_Wtime();
+        double local = t1 - t0, global = 0.0;
+        MPI_Reduce(&local, &global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            double smax = 0.0;
+            for (double s : S) if (s > smax) smax = s;
+            std::cout << m << " " << n << " " << size << " " << p << " " << q
+                      << " " << nb << " " << global << " " << smax << std::endl;
+        }
     }
 
     MPI_Finalize();
     return 0;
 }
-
 
