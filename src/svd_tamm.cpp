edec867897c63be59224bbf9b4941d38624e54e2
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-08-21 22:44:11 -0700

    Fast Push for Uploading Code to Remote

--- src/svd_tamm.cpp
+++ src/svd_tamm.cpp
@@ -149,99 +149,76 @@ static double time_slate_svds(int64_t N, int n_svd, tamm::ProcGroup world_pg) {
     return (world_pg.rank().value() == 0 ? (t1 - t0) : 0.0);
 }
 
-static double time_eigen_svds(int64_t N, int n_svd, tamm::ProcGroup world_pg) {
-    tamm::AtomicCounterGA ac{world_pg, 1};
-    ac.allocate(0);
+static double time_eigen_svds_host(int64_t N, int n_svd, tamm::ProcGroup world_pg) {
     world_pg.barrier();
     double t0 = 0.0, t1 = 0.0;
     if (world_pg.rank().value() == 0) {
         t0 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-    }
-    const int64_t n = 2 * N;
-    while (true) {
-        int64_t idx = ac.fetch_add(0, 1);
-        if (idx >= n_svd) break;
-        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> A(n, n);
-        for (int64_t j = 0; j < n; ++j) {
-            for (int64_t i = 0; i < n; ++i) {
-                double x = double(i);
-                double y = double(j);
-                A(i, j) = std::sin(0.001 * (x + 3.0 * y));
+        const int64_t n = 2 * N;
+        for (int i = 0; i < n_svd; ++i) {
+            Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> A(n, n);
+            for (int64_t j = 0; j < n; ++j) {
+                for (int64_t k = 0; k < n; ++k) {
+                    double x = double(k);
+                    double y = double(j);
+                    A(k, j) = std::sin(0.001 * (x + 3.0 * y));
+                }
             }
+            Eigen::BDCSVD<Eigen::MatrixXd> svd(A, 0);
         }
-        Eigen::BDCSVD<Eigen::MatrixXd> svd(A, 0);
-    }
-    world_pg.barrier();
-    if (world_pg.rank().value() == 0) {
         t1 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     }
-    ac.deallocate();
+    world_pg.barrier();
     return (world_pg.rank().value() == 0 ? (t1 - t0) : 0.0);
 }
 
-static double time_itensor_contractions_queue(int64_t N, int n_contr, tamm::ProcGroup world_pg) {
-    tamm::AtomicCounterGA ac{world_pg, 1};
-    ac.allocate(0);
+static double time_itensor_contractions_host(int64_t N, int n_contr, tamm::ProcGroup world_pg) {
     world_pg.barrier();
     double t0 = 0.0, t1 = 0.0;
     if (world_pg.rank().value() == 0) {
         t0 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-    }
-    while (true) {
-        int64_t idx = ac.fetch_add(0, 1);
-        if (idx >= n_contr) break;
-        size_t M = static_cast<size_t>(N);
-        size_t bt = std::min(M, size_t(64));
-        itensor::Index l(int(M), "l");
-        itensor::Index p1(2, "p1");
-        itensor::Index p2(2, "p2");
-        itensor::Index b(int(bt), "b");
-        itensor::Index r(int(M), "r");
-        itensor::ITensor A(l, p1, b), B(b, p2, r), C(l, p1, p2, r);
-        A.fill(1.0);
-        B.fill(1.0);
-        C = A * B;
-    }
-    world_pg.barrier();
-    if (world_pg.rank().value() == 0) {
+        for (int i = 0; i < n_contr; ++i) {
+            size_t M = static_cast<size_t>(N);
+            size_t bt = std::min(M, size_t(64));
+            itensor::Index l(int(M), "l");
+            itensor::Index p1(2, "p1");
+            itensor::Index p2(2, "p2");
+            itensor::Index b(int(bt), "b");
+            itensor::Index r(int(M), "r");
+            itensor::ITensor A(l, p1, b), B(b, p2, r), C(l, p1, p2, r);
+            A.fill(1.0);
+            B.fill(1.0);
+            C = A * B;
+        }
         t1 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     }
-    ac.deallocate();
+    world_pg.barrier();
     return (world_pg.rank().value() == 0 ? (t1 - t0) : 0.0);
 }
 
-static double time_itensor_svds(int64_t N, int n_svd, tamm::ProcGroup world_pg) {
-    tamm::AtomicCounterGA ac{world_pg, 1};
-    ac.allocate(0);
+static double time_itensor_svds_host(int64_t N, int n_svd, tamm::ProcGroup world_pg) {
     world_pg.barrier();
     double t0 = 0.0, t1 = 0.0;
     if (world_pg.rank().value() == 0) {
         t0 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-    }
-    int64_t n = 2 * N;
-    while (true) {
-        int64_t idx = ac.fetch_add(0, 1);
-        if (idx >= n_svd) break;
-        itensor::Index x(int(n), "x");
-        itensor::Index y(int(n), "y");
-        itensor::ITensor A(x, y);
-        for (int64_t j = 1; j <= n; ++j) {
-            for (int64_t i = 1; i <= n; ++i) {
-                double xr = double(i - 1);
-                double yr = double(j - 1);
-                A.set(i, j, std::sin(0.001 * (xr + 3.0 * yr)));
+        int64_t n = 2 * N;
+        for (int i = 0; i < n_svd; ++i) {
+            itensor::Index x(int(n), "x");
+            itensor::Index y(int(n), "y");
+            itensor::ITensor A(x, y);
+            for (int64_t jj = 1; jj <= n; ++jj) {
+                for (int64_t ii = 1; ii <= n; ++ii) {
+                    double xr = double(ii - 1);
+                    double yr = double(jj - 1);
+                    A.set(ii, jj, std::sin(0.001 * (xr + 3.0 * yr)));
+                }
             }
+            auto [U, S, V] = itensor::svd(A, {x}, {y});
+            (void)U; (void)S; (void)V;
         }
-        auto [U, S, V] = itensor::svd(A, {x}, {y});
-        (void)U;
-        (void)S;
-        (void)V;
-    }
-    world_pg.barrier();
-    if (world_pg.rank().value() == 0) {
         t1 = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     }
-    ac.deallocate();
+    world_pg.barrier();
     return (world_pg.rank().value() == 0 ? (t1 - t0) : 0.0);
 }
 
@@ -271,7 +248,7 @@ int main(int argc, char** argv) {
                   << " minN " << minN << " maxN " << maxN
                   << " step " << step << " csv " << csv << std::endl;
         std::ofstream ofs(csv, std::ios::out | std::ios::trunc);
-        ofs << "bond_dim,t_contr_queue,t_contr_batch,t_svd_slate,t_svd_eigen,t_contr_itensor,t_svd_itensor,t_total\n";
+        ofs << "bond_dim,t_contr_queue,t_contr_batch,t_svd_slate,t_svd_eigen_host,t_contr_itensor_host,t_svd_itensor_host,t_total\n";
         ofs.close();
     }
 
@@ -288,17 +265,17 @@ int main(int argc, char** argv) {
         double t_slate = time_slate_svds(N, n_svd, world_pg);
         if (rank == 0) std::cout << "N " << N << " svd slate done " << std::fixed << std::setprecision(6) << t_slate << " s" << std::endl;
 
-        if (rank == 0) std::cout << "N " << N << " svd eigen start" << std::endl;
-        double t_eigen = time_eigen_svds(N, n_svd, world_pg);
-        if (rank == 0) std::cout << "N " << N << " svd eigen done " << std::fixed << std::setprecision(6) << t_eigen << " s" << std::endl;
+        if (rank == 0) std::cout << "N " << N << " svd eigen host start" << std::endl;
+        double t_eigen = 0;//time_eigen_svds_host(N, n_svd, world_pg);
+        if (rank == 0) std::cout << "N " << N << " svd eigen host done " << std::fixed << std::setprecision(6) << t_eigen << " s" << std::endl;
 
-        if (rank == 0) std::cout << "N " << N << " itensor contractions start" << std::endl;
-        double t_it_contr = time_itensor_contractions_queue(N, n_contr, world_pg);
-        if (rank == 0) std::cout << "N " << N << " itensor contractions done " << std::fixed << std::setprecision(6) << t_it_contr << " s" << std::endl;
+        if (rank == 0) std::cout << "N " << N << " itensor contractions host start" << std::endl;
+        double t_it_contr = time_itensor_contractions_host(N, n_contr, world_pg);
+        if (rank == 0) std::cout << "N " << N << " itensor contractions host done " << std::fixed << std::setprecision(6) << t_it_contr << " s" << std::endl;
 
-        if (rank == 0) std::cout << "N " << N << " itensor svd start" << std::endl;
-        double t_it_svd = time_itensor_svds(N, n_svd, world_pg);
-        if (rank == 0) std::cout << "N " << N << " itensor svd done " << std::fixed << std::setprecision(6) << t_it_svd << " s" << std::endl;
+        if (rank == 0) std::cout << "N " << N << " itensor svd host start" << std::endl;
+        double t_it_svd = time_itensor_svds_host(N, n_svd, world_pg);
+        if (rank == 0) std::cout << "N " << N << " itensor svd host done " << std::fixed << std::setprecision(6) << t_it_svd << " s" << std::endl;
 
         if (rank == 0) {
             double t_total = t_contr_q + t_slate;
@@ -314,6 +291,7 @@ int main(int argc, char** argv) {
             ofs.close();
             std::cout << "N " << N << " total " << std::fixed << std::setprecision(6) << t_total << " s" << std::endl;
         }
+        world_pg.barrier();
     }
 
     if (rank == 0) std::cout << "done" << std::endl;
